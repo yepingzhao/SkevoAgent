330d9c9fda5a1cb2ec900aed145e8e7137b42f6b
Yeping Zhao <zyp2606841563@gmail.com>
2026-04-28 16:31:10 +0800

    chore: initialize BearCode project

---
+++ agents/agent.py
@@ -0,0 +1,71 @@
+#!/usr/bin/env python3
+from __future__ import annotations
+
+import asyncio
+import uuid
+import time
+from typing import Callable, Awaitable
+
+from agents.tools import ToolDef, tool_definitions
+
+
+MODEL_CONTEXT = {
+    "claude-opus-4-6": 200000,
+    "claude-sonnet-4-6": 200000,
+    "claude-sonnet-4-20250514": 200000,
+    "claude-haiku-4-5-20251001": 200000,
+    "claude-opus-4-20250514": 200000,
+    "gpt-4o": 128000,
+    "gpt-4o-mini": 128000,
+    "deepseek-chat":200000
+}
+
+def _get_context_windows(model:str)->int:
+    return MODEL_CONTEXT.get(model, 200000)
+
+class Agent:
+    def __init__(self,
+                 *,
+                 permission_mode:str="default",
+                 model:str="deepseek-chat",
+                 api_base: str | None=None,
+                 anthropic_base_url: str | None=None,
+                 api_key: str | None=None,
+                 thinking: bool=False,
+                 max_cost_usd: float | None=None,
+                 max_turns: int | None=None,
+                 confirm_fn:Callable[[str], Awaitable[bool]] | None=None,
+                 custom_system_prompt: str | None=None,
+                 custom_tools: list[ToolDef] | None=None,
+                 is_sub_agent: bool=False,):
+        self.permission_mode = permission_mode
+        self.thinking = thinking
+        self.tools = custom_tools or tool_definitions
+        self.max_cost_usd = max_cost_usd
+        self.max_turns = max_turns
+        self.confirm_fn = confirm_fn
+        self.effective_windos=_get_context_windows(model) -20000
+        self.session_id = uuid.uuid4().hex[:8]
+        self.session_start_time= time.strftime("%Y-%m-%dT%H:%M:%SZ",time.gmtime())
+        self.total_input_tokens = 0
+        self.total_output_tokens = 0
+        self.last_input_tokens = 0
+        self.current_turns = 0
+        self.last_api_call_time = 0
+
+        self._abort = False
+        #存储异步任务
+        self._current_task:asyncio.Task | None = None
+        #权限白名单
+        self._confirmed_paths: set[str] = set()
+
+
+        # 计划模式”（Plan Mode）状态的变量
+        self._pre_plan_mode: str | None=None
+        self._plan_file_path: str | None=None
+        self._plan_approval_fn : Callable[[str], Awaitable[bool]] | None=None
+        self._context_cleared : bool=False
+
+        #思考模式
+        self._thinking_mode = self._
+        self.api_base = api_base
---
+++ agents/tools.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+
+
+ToolDef = dict  # Anthropic tool schema dict
+
+
+#工具定义
+tool_definitions: list[ToolDef] = []
